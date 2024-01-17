9166542082a2d60b61b8d5dd2c2041329eb62f00
Ahmad Wahana Jaya <119087167+Wannjayy@users.noreply.github.com>
2024-01-18 05:13:11 +0700

    gas

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector("#show-login").addEventListener("click", function(){
+        document.querySelector(".popup-signup").classList.remove("active");
+        document.querySelector(".popup").classList.add("active");
+    });
+
+    document.querySelector(".popup .close-btn").addEventListener("click", function(){
+        document.querySelector(".popup").classList.remove("active");
+    });
+
+    document.querySelector("#show-signup").addEventListener("click", function(){
+        document.querySelector(".popup").classList.remove("active");
+        document.querySelector(".popup-signup").classList.add("active");
+    });
+
+    document.querySelector(".popup-signup .close-btn").addEventListener("click", function(){
+        document.querySelector(".popup-signup").classList.remove("active");
+    });
+});
